0fc05644706833f6a35967bf83bb09e0cb5e5566
Kinga <kinga.sabat@gmail.com>
2025-07-28 21:01:54 +0200

    added Nativewind

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,5 @@
 import { Text, View } from "react-native";
+import "../global.css";
 
 export default function Index() {
   return (
@@ -10,6 +11,13 @@ export default function Index() {
       }}
     >
       <Text>Edit app/index.tsx to edit this screen.</Text>
+
+      <View className="flex-1 items-center justify-center bg-white">
+        <Text className="text-xl font-bold text-blue-500">
+          Welcome to Nativewind!
+        </Text>
+      </View>
+      
     </View>
   );
 }
