7236b8e15ad2d6aee5a06ac96f6354bd2092c933
Kinga <kinga.sabat@gmail.com>
2025-08-13 22:03:56 +0200

    added SafeAreaView & StatusBar color

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,16 @@
 import { Stack } from "expo-router";
+import { StatusBar } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 import { CounterProvider } from "./context/counter-context";
 
 export default function RootLayout() {
   return ( 
     <CounterProvider>
-      <Stack />
+      <StatusBar translucent={false} backgroundColor="#1c1c1c" barStyle="light-content" />
+
+      <SafeAreaView style={{ flex: 1 }}>
+        <Stack screenOptions={{ headerShown: false }} />
+      </SafeAreaView>
     </CounterProvider>
   )
 }
