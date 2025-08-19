fd0a9640929cebe09d864b67f197ad40c3d57ed0
Kinga <kinga.sabat@gmail.com>
2025-08-19 22:32:50 +0200

    prevent rendering before fonts are loaded

--- app/index.tsx
+++ app/index.tsx
@@ -19,7 +19,7 @@ import { fetchQuestPartsList } from './utils/questPartsList';
 const { width, height } = Dimensions.get('window');
 
 export default function Index() {
-  const [loaded] = useFonts({
+  const [fontsLoaded] = useFonts({
     Tomorrow: Tomorrow_400Regular,
     Saira: SairaExtraCondensed_400Regular,
   });
@@ -49,14 +49,14 @@ export default function Index() {
 
   // hide overlay only when everything is ready
   useEffect(() => {
-    if (loaded && !loading && minDelayPassed) {
+    if (fontsLoaded && !loading && minDelayPassed) {
       Animated.timing(overlayOpacity, {
         toValue: 0,
         duration: 400,
         useNativeDriver: true,
       }).start(() => setShowOverlay(false));
     }
-  }, [loaded, loading, minDelayPassed]);
+  }, [fontsLoaded, loading, minDelayPassed]);
 
   useEffect(() => {
     const fetchPartsList = async () => {
@@ -113,103 +113,107 @@ export default function Index() {
         </Animated.View>
       )}
 
-      <ScrollView className='bg-[#1c1c1c]'>
-        <View className='flex justify-center'>
-          <View className='flex flex-row justify-between px-4 py-8'>
-            <CustomButton
-              title="Back"
-              onPress={handlePrev}
-              disabled={questPartIndex === 0}
-            />
-            <CustomButton
-              title="Next"
-              onPress={handleNext}
-              disabled={questPartIndex === (data?.parts.length ?? 1) - 1}
-            />
-          </View>
-
-          <View className='flex flex-row items-center px-4 py-8'>
-            {Platform.OS === 'web' ? (
-              <>
-                <Pressable
-                  onPress={toggleSwitch}
-                  style={[
-                    styles.webSwitch,
-                    { backgroundColor: isCompleted ? '#767577' : '#767577' },
-                  ]}
-                >
-                  <View
-                    style={[
-                      styles.thumb,
-                      {
-                        left: isCompleted ? 24 : 2,
-                        backgroundColor: isCompleted ? '#f4f3f4' : '#f4f3f4',
-                      },
-                    ]}
-                  />
-                </Pressable>
-                <Pressable onPress={toggleSwitch}>
-                  <CustomText className='text-white text-xl ml-2'>{`Mark as ${isCompleted ? 'incomplete' : 'complete'}`}</CustomText>
-                </Pressable>
-              </>
-            ) : (
-
-              <>
-                <Switch
-                  value={isCompleted}
-                  onValueChange={toggleSwitch}
-                  trackColor={{ false: '#767577', true: '#767577' }}
-                  thumbColor={isCompleted ? '#f4f3f4' : '#f4f3f4'}
-                  ios_backgroundColor="#3e3e3e"
+      {(fontsLoaded && !loading) && (
+        <View style={{ flex: 1 }}>
+          <ScrollView className='bg-[#1c1c1c]'>
+            <View className='flex justify-center'>
+              <View className='flex flex-row justify-between px-4 py-8'>
+                <CustomButton
+                  title="Back"
+                  onPress={handlePrev}
+                  disabled={questPartIndex === 0}
                 />
-                <Pressable onPress={toggleSwitch}>
-                  <CustomText className='text-white text-xl ml-2'>{`Mark as ${isCompleted ? 'incomplete' : 'complete'}`}</CustomText>
-                </Pressable>
-              </>
-            )}
+                <CustomButton
+                  title="Next"
+                  onPress={handleNext}
+                  disabled={questPartIndex === (data?.parts.length ?? 1) - 1}
+                />
+              </View>
+
+              <View className='flex flex-row items-center px-4 py-8'>
+                {Platform.OS === 'web' ? (
+                  <>
+                    <Pressable
+                      onPress={toggleSwitch}
+                      style={[
+                        styles.webSwitch,
+                        { backgroundColor: isCompleted ? '#767577' : '#767577' },
+                      ]}
+                    >
+                      <View
+                        style={[
+                          styles.thumb,
+                          {
+                            left: isCompleted ? 24 : 2,
+                            backgroundColor: isCompleted ? '#f4f3f4' : '#f4f3f4',
+                          },
+                        ]}
+                      />
+                    </Pressable>
+                    <Pressable onPress={toggleSwitch}>
+                      <CustomText className='text-white text-xl ml-2'>{`Mark as ${isCompleted ? 'incomplete' : 'complete'}`}</CustomText>
+                    </Pressable>
+                  </>
+                ) : (
+
+                  <>
+                    <Switch
+                      value={isCompleted}
+                      onValueChange={toggleSwitch}
+                      trackColor={{ false: '#767577', true: '#767577' }}
+                      thumbColor={isCompleted ? '#f4f3f4' : '#f4f3f4'}
+                      ios_backgroundColor="#3e3e3e"
+                    />
+                    <Pressable onPress={toggleSwitch}>
+                      <CustomText className='text-white text-xl ml-2'>{`Mark as ${isCompleted ? 'incomplete' : 'complete'}`}</CustomText>
+                    </Pressable>
+                  </>
+                )}
+              </View>
+
+              {data && data.parts && (
+                <GestureDetector gesture={swipeGesture}>
+                  <ScrollView style={{ flex: 1 }} contentContainerStyle={{ paddingBottom: 20 }}>
+                    <CustomTitleText className='text-4xl text-white font-bold flex justify-center mb-6 px-4'>
+                      Part {data.parts[questPartIndex]}
+                    </CustomTitleText>
+
+                    <Table partId={data.parts[questPartIndex]} />
+                  </ScrollView>
+                </GestureDetector>
+              )}
+
+            </View>
+
+            <Svg width={width} height={height} style={{ position: 'absolute', zIndex: -1, opacity: 0.01 }}>
+              <Defs>
+                <Pattern
+                  id="stripes"
+                  patternUnits="userSpaceOnUse"
+                  width="20"
+                  height="20"
+                  patternTransform="rotate(45)"
+                >
+                  <Path d="M0 0 L20 0 L20 10 L0 10 Z" fill="#FFC0CB" />
+                </Pattern>
+              </Defs>
+              <Rect width="100%" height="100%" fill="url(#stripes)" />
+            </Svg>
+
+          </ScrollView>
+
+          <View style={styles.gradientWrapper} pointerEvents='none'>
+            <LinearGradient
+              colors={ isCompleted ? ['rgba(26, 123, 55, 0.35)', 'transparent'] : ['rgba(136, 74, 28, 0.35)', 'transparent']
+              }
+              start={{ x: 0.5, y: 1 }}
+              end={{ x: 0.5, y: 0 }}
+              locations={[0, 0.7]}
+              style={styles.gradientOverlay}
+            />
           </View>
-
-          {data && data.parts && (
-            <GestureDetector gesture={swipeGesture}>
-              <ScrollView style={{ flex: 1 }} contentContainerStyle={{ paddingBottom: 20 }}>
-                <CustomTitleText className='text-4xl text-white font-bold flex justify-center mb-6 px-4'>
-                  Part {data.parts[questPartIndex]}
-                </CustomTitleText>
-
-                <Table partId={data.parts[questPartIndex]} />
-              </ScrollView>
-            </GestureDetector>
-          )}
-
         </View>
-
-        <Svg width={width} height={height} style={{ position: 'absolute', zIndex: -1, opacity: 0.01 }}>
-          <Defs>
-            <Pattern
-              id="stripes"
-              patternUnits="userSpaceOnUse"
-              width="20"
-              height="20"
-              patternTransform="rotate(45)"
-            >
-              <Path d="M0 0 L20 0 L20 10 L0 10 Z" fill="#FFC0CB" />
-            </Pattern>
-          </Defs>
-          <Rect width="100%" height="100%" fill="url(#stripes)" />
-        </Svg>
-
-      </ScrollView>
-
-      <View style={styles.gradientWrapper} pointerEvents='none'>
-        <LinearGradient
-          colors={ isCompleted ? ['rgba(26, 123, 55, 0.35)', 'transparent'] : ['rgba(136, 74, 28, 0.35)', 'transparent']
-          }
-          start={{ x: 0.5, y: 1 }}
-          end={{ x: 0.5, y: 0 }}
-          locations={[0, 0.7]}
-          style={styles.gradientOverlay}
-        />
-      </View>
+      )}
 
     </View>
   );
