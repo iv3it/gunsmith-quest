229f71075bd7c9da8009e820e7549c51a63a8deb
Kinga <kinga.sabat@gmail.com>
2025-09-17 14:10:53 +0200

    credits page update

--- app/credits.tsx
+++ app/credits.tsx
@@ -1,3 +1,4 @@
+import * as Linking from 'expo-linking';
 import { View } from "react-native";
 import CustomText from "./components/custom-text";
 import CustomTitleText from "./components/custom-title-text";
@@ -5,10 +6,19 @@ import CustomTitleText from "./components/custom-title-text";
 export default function CreditsPage() {
   return (
     <View className="flex-1 bg-[#1c1c1c] px-4 pt-4">
-      <CustomTitleText className='text-4xl text-white md:text-center mb-6'>
+      <CustomTitleText className='text-4xl text-white md:text-center my-6'>
         Credits
       </CustomTitleText>
-      <CustomText className="text-white text-xl">test</CustomText>
+
+      <CustomText className="text-white text-2xl mb-4">Creators:</CustomText>
+      <CustomText className="mb-12">
+        <CustomText className="text-white underline underline-offset-4 text-xl" onPress={() => Linking.openURL('https://github.com/Fyffe')}>Fyffe</CustomText>
+        <CustomText className="text-white text-xl"> & </CustomText>
+        <CustomText className="text-white underline underline-offset-4 text-xl" onPress={() => Linking.openURL('https://github.com/iv3it')}>iv3it</CustomText>
+      </CustomText>
+
+      <CustomText className="text-white text-2xl mb-4">Sources:</CustomText>
+      <CustomText className="text-white underline underline-offset-4 text-xl mb-2" onPress={() => Linking.openURL('https://www.flaticon.com/free-icon/rifle_4810861')}>Rifle icon created by Triangle Squad - Flaticon</CustomText>
     </View>
   );
 }
